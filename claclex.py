08a303e724de6e8eef97d33e0db201a0fd2fb7cb
Aayush Sahay <47693269+Chemboy007@users.noreply.github.com>
2021-04-25 15:26:43 +0530

    SDT
    Can handle:
    type check with types int, float, char, boolean ( including type checking with constants)
    uniqueness detection
    
    Cannot handle:
    declaration and definations at the same line like: "int a = 10;"
    implicit type conversions

--- claclex.py
+++ claclex.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------
-# 
-# data_type: int(INT), char(CHR), string(STR), float(FLT), array, bool(BOOL), 
+#
+# data_type: int(INT), char(CHR), string(STR), float(FLT), array, bool(BOOL),
 # keyword: print, exit, (include), for, with, while, if - else, {continue}, {break}, and, or, true, false, return
 # arthmetic operator "-"|"*"|"+"|"/"|"%"(MOD)
 # rln operator "=="(DEQUAL) | "!"(not equal to)(NOT) | ">"(GTHEN) | "<"(LTHEN)
@@ -12,65 +12,66 @@
 # variable (IDVAR)
 # comments #
 # ------------------------------------------------------------
-
- 
+# counter[0] = loop counter
+counter = [0]
+# when introducing break, check of counter[0] is > 0 and reduce counter[0] by one
 import ply.lex as lex
 
 # List of token names.   This is always required
 tokens = (
-   'INT',
-   'FLT',
-   'STR',
-   'CHR',
-   'EQUAL',
-   'SCOLON',
-   'BOOL',
-   'TRUE',
-   'FALSE',
-   'INTEGER',
-   'FLOAT',
-   'STRING',
-   'CHAR',
-   'IDVAR',
-   'LBB',
-   'RBB',
-   'LCB',
-   'RCB',
-   'LPAREN',
-   'RPAREN',
-   'PRINT',
-   'EXIT',
-   'COM',
-   'FOR',
-   'WHILE',
-   'WITH',
-   'IF',
-   'ELSE',
-   'AND',
-   'OR',
-   'PLUS',
-   'MINUS',
-   'MUL',
-   'DIV',
-   'MOD',
-   'DEQUAL',
-   'NOT',
-   'LTHEN',
-   'GTHEN',
-   'COMMENT',
-   'MAIN',
-   'VOID',
-   'RETURN'
+    'INT',
+    'FLT',
+    'STR',
+    'CHR',
+    'EQUAL',
+    'SCOLON',
+    'BOOL',
+    'TRUE',
+    'FALSE',
+    'INTEGER',
+    'FLOAT',
+    'STRING',
+    'CHAR',
+    'IDVAR',
+    'LBB',
+    'RBB',
+    'LCB',
+    'RCB',
+    'LPAREN',
+    'RPAREN',
+    'PRINT',
+    'EXIT',
+    'COM',
+    'FOR',
+    'WHILE',
+    'WITH',
+    'IF',
+    'ELSE',
+    'AND',
+    'OR',
+    'PLUS',
+    'MINUS',
+    'MUL',
+    'DIV',
+    'MOD',
+    'DEQUAL',
+    'NOT',
+    'LTHEN',
+    'GTHEN',
+    'COMMENT',
+    'MAIN',
+    'VOID',
+    'RETURN'
 )
 
 # Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_MUL   = r'\*'
-t_DIV  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_EQUAL   = r'='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MUL = r'\*'
+t_DIV = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_EQUAL = r'='
 t_SCOLON = r';'
 t_MOD = r'%'
 t_DEQUAL = r'=='
@@ -86,113 +87,140 @@ t_COM = r','
 
 def t_INT(t):
     r'int'
+    # t.value = "INTEGER"
     return t
+
 
 def t_MAIN(t):
     r'main'
     return t
 
+
 def t_VOID(t):
     r'void'
     return t
+
 
 def t_CHR(t):
     r'char'
     return t
 
+
 def t_STR(t):
     r'string'
     return t
+
 
 def t_BOOL(t):
     r'boolean'
     return t
 
+
 def t_FLT(t):
     r'float'
     return t
+
 
 def t_PRINT(t):
     r'print'
     return t
 
+
 def t_EXIT(t):
     r'exit'
     return t
 
+
 def t_FOR(t):
     r'for'
+    counter[1] += 1
     return t
+
 
 def t_WITH(t):
     r'with'
     return t
 
+
 def t_WHILE(t):
     r'while'
+    counter[1] += 1
     return t
+
 
 def t_IF(t):
     r'if'
     return t
 
+
 def t_ELSE(t):
     r'else'
     return t
+
 
 def t_AND(t):
     r'and'
     return t
 
+
 def t_OR(t):
     r'or'
     return t
+
 
 def t_RETURN(t):
     r'return'
     return t
 
+
 def t_TRUE(t):
     r'true'
-    t.value = 1
+    t.value = {'value': 1, 'type': "boolean"}
     return t
+
 
 def t_FALSE(t):
     r'false'
-    t.value = 0
+    t.value = {'value': 0, 'type': "boolean"}
     return t
+
 
 def t_IDVAR(t):
     r'[_a-zA-Z][_a-zA-Z0-9]*'
+    t.value = {'value': t.value, 'type': "ID"}
     return t
+
 
 # A regular expression rule with some action code
 
 def t_FLOAT(t):
     r'[+-]?[0-9]+\.[0-9]+'
-    t.value = float(t.value)    
+    t.value = {"value":float(t.value), "type": "float"}
     return t
 
 
 def t_INTEGER(t):
     r'\d+'
-    t.value = int(t.value)    
+    t.value = {"value": int(t.value), "type": "int"}
     return t
+
 
 def t_CHAR(t):
     r'\'([^\\\n]|(\\.))*?\''
-    if(len(t.value) == 2):
+    if (len(t.value) == 2):
         print("InValid Char")
-    t.value = ord(t.value[-2])
+        exit()
+    t.value = t.value = {"value": ord(t.value[-2]), "type": "char"}
     return t
 
 
-# C or C++ comment (ignore)    
+# C or C++ comment (ignore)
 def t_COMMENT(t):
     r'\#.*'
     pass
 
- # C string
+
+# C string
 def t_STRING(t):
     r'\"([^\\\n]|(\\.))*?\"'
     return t
@@ -201,23 +229,28 @@ def t_STRING(t):
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
+    counter[0] += 1
     t.lexer.lineno += len(t.value)
 
+
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t\n'
+t_ignore = ' \t\n'
+
 
 # Error handlingf rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 # Build the lexer
 lexer = lex.lex()
- # Test it out
-# data = '''
+# Test it out
+# data = "int a;"
+# '''
 # int[] a = {4,5,6};
-# < > + [] () { } ; 12.23 if else print exit 
-# 12Sds 
+# < > + [] () { } ; 12.23 if else print exit
+# 12Sds
 # _1 %1
 # 1false
 # "ayush agarwal"
@@ -230,6 +263,6 @@ lexer = lex.lex()
 # # Tokenize
 # while True:
 #     tok = lexer.token()
-#     if not tok: 
+#     if not tok:
 #         break      # No more input
 #     print(tok)
--- clacyacc.py
+++ clacyacc.py
@@ -2,7 +2,7 @@
     line -> nline line | nline
     nline -> dtype var SCOLON | STR var SCOLON
     nline ->  fline
-    fline : dtype numexp | numexp  
+    fline : dtype numexp | numexp
     numexp -> assign SCOLON
     exp -> exp PLUS term | exp MINUS term | term
     term -> term MUL fact | term DIV fact | term MOD fact | fact
@@ -13,7 +13,7 @@
     nline -> STR IDVAR EQUAL strvar SCOLON
     strvar -> STRING | var
 
-    nline : BOOL MAIN LPAREN RPAREN LCB line RCB 
+    nline : BOOL MAIN LPAREN RPAREN LCB line RCB
     nline : RETURN LPAREN bval RPAREN SCOLON
     nline : EXIT LPAREN RPAREN SCOLON
     nline : PRINT LPAREN pline RPAREN SCOLON
@@ -27,24 +27,25 @@
     bexp2 : bexp2 rln exp | exp
     rln : DEQUAL | GTHEN | LTHEN | NOT
     andor : AND | OR
-    
+
     empty :
 
     nline : FOR LPAREN fline bexp SCOLON assign RPAREN LCB line RCB
     nline : WHILE LPAREN bexp RPAREN LCB line RCB
     assign : var EQUAL exp
 
-    
 
-    nline : dtype LBB RBB IDVAR EQUAL arrt SCOLON | STR LBB RBB IDVAR EQUAL arrt SCOLON 
+
+    nline : dtype LBB RBB IDVAR EQUAL arrt SCOLON | STR LBB RBB IDVAR EQUAL arrt SCOLON
     arrt : var | LCB dws RCB
     dws : factarr COM dws | factarr
     factarr : fact | STRING
 '''
-
+import sym
 import ply.yacc as yacc
 from claclex import tokens
- 
+
+
 
 vars = {}
 pyth = False
@@ -59,20 +60,22 @@ pyth = False
 
 
 def p_line_chg(p):
-    '''line : nline line 
+    '''line : nline line
             | nline'''
 
 def p_nline_init(p):
     '''nline : dtype var SCOLON
                 | STR var SCOLON'''
-    
+    sym.store(p[2]['value'], p[1])
+
+
 def p_nline_dec(p):
     'nline : fline'
 
 def p_nline_str(p):
     'nline : STR IDVAR EQUAL strvar SCOLON'
     vars[p[2]] = p[4]
-    
+
 def p_nline_main(p):
     'nline : BOOL MAIN LPAREN RPAREN LCB line RCB'
 
@@ -92,41 +95,75 @@ def p_pline_print(p):
     '''pline : exp
             | STRING'''
 
+
 def p_nline_for(p):
-    'nline : FOR LPAREN fline bexp SCOLON assign RPAREN LCB line RCB'
+    'nline : FOR LPAREN fline bexp SCOLON assign RPAREN sps line RCB'
+    sym.remove()
+
 
 def p_nline_while(p):
-    'nline : WHILE LPAREN bexp RPAREN LCB line RCB'
+    'nline : WHILE LPAREN bexp RPAREN sps  line RCB'
+    sym.remove()
+
+def p_sps(p):
+    'sps : LCB'
+    sym.add_scopes()
+
 
 def p_ifsts_fst(p):
     'ifsts : IF LPAREN bexp RPAREN LCB line RCB elsests'
 
+
 def p_elsests_fst(p):
-    '''elsests : ELSE LCB line RCB 
-                | ELSE ifsts 
+    '''elsests : ELSE LCB line RCB
+                | ELSE ifsts
                 | empty'''
 
 def p_numexp_stm(p):
     'numexp : assign SCOLON'
-    
+
 
 def p_exp_pm(p):
-    '''exp : exp PLUS term 
-           | exp MINUS term 
+    '''exp : exp PLUS term
+           | exp MINUS term
            | term'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        if p[1]['type'] == p[3]['type']:
+            p[0] = {'value':None, 'type': p[1]['type']}
+        else:
+            print("type mismatch")
+            exit()
+
 
 def p_term_mdm(p):
-    '''term : term MUL fact 
-            | term DIV fact 
-            | term MOD fact 
+    '''term : term MUL fact
+            | term DIV fact
+            | term MOD fact
             | fact'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        if p[1]['type'] == p[3]['type']:
+            p[0] = {'value':None, 'type': p[1]['type']}
+        else:
+            print("type mismatch")
+            exit()
+
 
 def p_fact_val(p):
-    '''fact : INTEGER 
-            | FLOAT 
+    '''fact : INTEGER
+            | FLOAT
             | var
             | bval
             | CHAR'''
+    if p[1]["type"] == "ID":
+        p[0] = sym.lookup(p[1]["value"])
+        # print(p[1])
+    else:
+        p[0] = p[1]
+
 
 def p_bval_val(p):
     '''bval : TRUE
@@ -136,34 +173,36 @@ def p_var_id(p):
     '''var : IDVAR
             | IDVAR LBB INTEGER RBB
             | IDVAR LBB IDVAR RBB'''
+
     p[0] = p[1]
 
 def p_dtype_num(p):
-    '''dtype : INT 
-            | FLT 
-            | CHR 
+    '''dtype : INT
+            | FLT
+            | CHR
             | BOOL'''
+    p[0] = p[1]
 
 def p_strvar_val(p):
-    '''strvar : STRING 
+    '''strvar : STRING
             | var'''
-    
+
 def p_bexp_fst(p):
-    '''bexp : bexp andor bexp2 
+    '''bexp : bexp andor bexp2
             | bexp2'''
 
 def p_bexp2_fst(p):
-    '''bexp2 : bexp2 rln exp 
+    '''bexp2 : bexp2 rln exp
             | exp'''
 
 def p_rln_fst(p):
-    '''rln : DEQUAL 
-            | GTHEN 
-            | LTHEN 
+    '''rln : DEQUAL
+            | GTHEN
+            | LTHEN
             | NOT'''
 
 def p_andor_fst(p):
-    '''andor : AND 
+    '''andor : AND
             | OR'''
 
 def p_empty(p):
@@ -172,9 +211,16 @@ def p_empty(p):
 
 def p_assign_num(p):
     'assign : var EQUAL exp'
+    p[1] = sym.lookup(p[1]["value"])
+    if p[1]["type"] == p[3]["type"]:
+        pass
+    else:
+        print("type mismatch")
+        exit()
+
 
 def p_fline_for(p):
-    '''fline : dtype numexp 
+    '''fline : dtype numexp
             | numexp'''
 
 
@@ -183,99 +229,17 @@ def p_nline_arr(p):
                 | STR LBB RBB IDVAR EQUAL arrt SCOLON'''
 
 def p_arrt_data(p):
-    '''arrt : var 
+    '''arrt : var
             | LCB dws RCB'''
 
 def p_dws_vals(p):
-    '''dws : factarr COM dws 
+    '''dws : factarr COM dws
             | factarr'''
-   
+
 def p_factarr_vals(p):
-    '''factarr : fact 
+    '''factarr : fact
     | STRING'''
 
-
-
-# def p_nlineP_print(p):
-#     'nlineP : PRINT LPAREN var RPAREN'
-#     print(vars[p[3]])
-
-# def p_nlineP_exit(p):
-#     'nlineP : EXIT LPAREN RPAREN'
-#     exit()
-
-# def p_line_rep(p):
-#     '''line : nline line
-#             | nline
-#             | pyt'''
-#     p[0] = p[1]
-
-
-# def p_nline_var(p):
-#     '''nline : INTEGER var EQUAL expression SEMICOLON'''
-#     if not p[2] in vars:
-#         vars[p[2]] = p[4]
-#         print(vars)
-
-# def p_nline_var_update(p):
-#     'nline : var EQUAL expression SEMICOLON'
-#     if p[1] in vars:
-#         vars[p[1]] = p[3]
-#         p[0] = p[3]
-
-# def p_nline_exit(p):
-#     'nline : EXIT LPAREN RPAREN SEMICOLON'
-#     exit()
-
-# def p_nline_print(p):
-#     'nline : PRINT LPAREN var RPAREN SEMICOLON'
-#     print(vars[p[3]])
-
-# def p_var_id(p):
-#     'var : IDVAR'
-#     p[0] = p[1]
-
-# def p_expression_var(p):
-#     'expression : var'
-#     p[0] = vars[p[1]]
-
-# def p_expression_plus(p):
-#     'expression : expression PLUS term'
-#     p[0] = p[1] + p[3]
-
-# def p_expression_minus(p):
-#     'expression : expression MINUS term'
-#     p[0] = p[1] - p[3]
-
-# def p_expression_term(p):
-#     'expression : term'
-#     p[0] = p[1]
-
-# def p_term_times(p):
-#     'term : term TIMES factor'
-#     p[0] = p[1] * p[3]
-
-# def p_term_div(p):
-#     'term : term DIVIDE factor'
-#     p[0] = p[1] / p[3]
-
-# def p_term_factor(p):
-#     'term : factor'
-#     p[0] = p[1]
-
-# def p_factor_num(p):
-#     '''factor : NUMBER
-#               | var'''
-#     if p[1] in vars:
-#         p[0] = vars[p[1]]
-#     elif type(p[1]) == type(0):
-#         p[0] = p[1]
-#     else:
-#         print('error')
-
-# def p_factor_expr(p):
-#     'factor : LPAREN expression RPAREN'
-#     p[0] = p[2]
 
 # Error rule for syntax errors
 def p_error(p):
@@ -294,7 +258,12 @@ parser = yacc.yacc()
 #    result = parser.parse(s)
 #    print(result)
 
-data = '''
+data = '''int a;
+        int b;
+       '''
+
+
+'''
 int[] arr = {1,"df",23, true, 12.32, 'w',asdf};
 int as[2];
 int[] asdf = {12};
@@ -336,8 +305,8 @@ exit();
 
 '''
 
-# 
-# 
+#
+#
 # '''
 ''' 
 int jav = 45;
